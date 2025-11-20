3d4f4b5c8a30895161fa9add47b6f6e4c846d490
Xiechengqi <Xiechengqi01@gmail.com>
2025-11-21 00:01:55 +0800

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,14 +2,17 @@
 
 name="chrome-novnc"
 docker rm -f ${name}
+
+# file
+# -p 5000:5000 \
+# terminal
+# -p 2222:2222 \
+
 docker run -itd \
   --restart=always \
   --privileged \
-  -p 5000:5000 \
-  -p 2222:2222 \
   -p 7900:7900 \
   -e NOVNC_PASSWORD="123123" \
   -e TERMINAL_PASSWORD="123123" \
   -v ${PWD}/scripts:/app/scripts \
-  --name ${name} \
-  fullnode/chrome-novnc:latest
+  --name ${name} chrome-novnc:latest
