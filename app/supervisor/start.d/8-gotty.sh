9e2557fced99b3b9fa9fceb7da1a5ca8102c5e83
root <root@homelab-gateway.tail71a2d0.ts.net>
2025-11-21 05:28:49 +0000

    add

--- app/supervisor/start.d/8-gotty.sh
+++ app/supervisor/start.d/8-gotty.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 [ ".${TERMINAL_USER}" != "." ] && [ ".${TERMINAL_PASSWORD}" != "." ] && OPTS="-c ${TERMINAL_USER}:${TERMINAL_PASSWORD}"
-/app/gotty/gotty -w -p 2222 ${OPTS} /bin/sh
+[ ".${TERMINAL_RPOXY}" != "." ] && export ALL_PROXY="${TERMINAL_RPOXY}"
+/app/gotty/gotty -w --once -p 2222 ${OPTS} /bin/sh
--- run.sh
+++ run.sh
@@ -5,14 +5,17 @@ BASEPATH=`dirname $(readlink -f ${BASH_SOURCE[0]})` && cd $BASEPATH
 name="chrome-novnc"
 docker rm -f ${name}
 
-# file
+## file port
 # -p 5000:5000 \
-# terminal
+## terminal port
 # -p 2222:2222 \
-# -e NOVNC_PASSWORD="123123" \
-# -e TERMINAL_USER="admin" \
-# -e TERMINAL_PASSWORD="123123" \
+## env
+# -e NOVNC_PASSWORD="123123"
+# -e TERMINAL_USER="admin"
+# -e TERMINAL_PASSWORD="123123"
 # -e CHROMIUM_PROXY_SERVER=socks5://192.168.1.15:1080
+# -e ALL_PROXY=socks5://192.168.1.15:1080
+# -e TERMINAL_RPOXY=socks5://192.168.1.15:1080
 
 docker run -itd \
   --restart=always \
